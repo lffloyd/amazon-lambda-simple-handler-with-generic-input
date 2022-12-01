f2081d1fb6dfc1b99a202b1ca9717086eb4bedcf
Luiz Matos <luiz.matos@amedigital.com>
2022-12-01 15:46:12 -0300

    feat: removes quarkus, leaving a plain lambda

--- src/main/java/org/test/lambda/GreetingLambda.java
+++ src/main/java/org/test/lambda/GreetingLambda.java
@@ -1,23 +1,30 @@
 package org.test.lambda;
 
-import org.jboss.logging.Logger;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GreetingLambda implements RequestHandler<Object, String> {
-    private static final Logger log = Logger.getLogger(GreetingLambda.class);
+    private LambdaLogger log = null;
     private String response = null;
 
     @Override
     public String handleRequest(Object input, Context context) {
-        log.info("Called 'handleReques'");
-        generateResponse(input);
-        return response;
+        log = context.getLogger();
+        try {
+            log.log(String.format("Called 'handleRequest': %s\n", new ObjectMapper().writeValueAsString(input)));
+            generateResponse(input);
+            return response;
+        } catch (Exception e) {
+            log.log(String.format("error: %s\n", e.getMessage()));
+            return null;
+        }
     }
 
     public void generateResponse(Object input) {
-        log.info("Called 'generateResponse'");
+        log.log("Called 'generateResponse'\n");
         response = "Hello!";
     }
 }
